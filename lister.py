aa229e5d077ce7d4cc623baa024bd307a14232ac
DJbistein <josjoh@kvn.no>
2023-09-20 12:49:23 +0200

    kuk

--- lister.py
+++ lister.py
@@ -1,5 +1,22 @@
 def hent_brukerdata():
+    # Returnerer en liste med tupler (brukernavn, passord)
     return [("jostein", "pass")]
+
+def legg_til_bruker(brukernavn, passord, brukerliste):
+    # Legger til en ny bruker til brukerlisten
+    brukerliste.append((brukernavn, passord))
+    return brukerliste
+
+def slett_bruker(brukernavn, brukerliste):
+    # Sletter en bruker fra brukerlisten
+    brukerliste = [bruker for bruker in brukerliste if bruker[0] != brukernavn]
+    return brukerliste
+
+def vis_brukernavn():
+    brukerliste = hent_brukerdata()
+    print("Brukernavn i systemet:")
+    for bruker in brukerliste:
+        print(bruker[0])
 
 def logg_inn():
     print("Logg inn:")
@@ -10,8 +27,20 @@ def logg_inn():
 
     if (brukernavn, passord) in brukerliste:
         print("Innlogging vellykket!")
+        svar = input("Ønsker du å slette din bruker? (ja/nei) ")
+        if svar.lower() == "ja":
+            brukerliste = slett_bruker(brukernavn, brukerliste)
+            print("Brukeren ble slettet.")
     else:
         print("Innlogging mislyktes. Feil brukernavn eller passord.")
+        svar = input("Ønsker du å opprette en ny konto? (ja/nei) ")
+        if svar.lower() == "ja":
+            if any(bruker[0] == brukernavn for bruker in brukerliste):
+                print("Brukernavnet eksisterer allerede. Velg et annet brukernavn.")
+            else:
+                brukerliste = legg_til_bruker(brukernavn, passord, brukerliste)
+                print("Ny bruker opprettet!")
 
 if __name__ == "__main__":
     logg_inn()
+    vis_brukernavn()
